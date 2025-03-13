87377246db9489d27a63aa80f49970d0ff607ae9
Ethan Noble <ethan.noble23@gmail.com>
2025-03-13 16:46:50 -0400

    feat

--- object_oriented_programming/rock_paper_scissors/oo_rock_paper_scissors_bonus_features.py
+++ object_oriented_programming/rock_paper_scissors/oo_rock_paper_scissors_bonus_features.py
@@ -1,7 +1,7 @@
 import random
 
 class Player:
-    CHOICES = ('rock', 'paper', 'scissors')
+    CHOICES = ('rock', 'paper', 'scissors', 'lizard', 'spock')
     WINNING_SCORE = 5
 
     def __init__(self):
@@ -25,7 +25,7 @@ class Human(Player):
         self.score = 0
 
     def choose(self):
-        prompt = "Please choose rock, paper, or scissors: "
+        prompt = "Please choose rock, paper, or scissors, lizard, or spock: "
 
         while True:
             choice = input(prompt).lower()
@@ -37,6 +37,14 @@ class Human(Player):
         self.move = choice
 
 class RPSGame:
+    WINNING_COMBINATIONS = {
+        'rock':     ['scissors',    'lizard'],
+        'paper':    ['rock',        'spock'],
+        'scissors': ['paper',       'lizard'],
+        'lizard':   ['paper',       'spock'],
+        'spock':    ['rock',        'scissors']
+    }
+
     def __init__(self):
         self._human = Human()
         self._computer = Computer()
@@ -45,26 +53,22 @@ class RPSGame:
         self._human.score = self._computer.score = 0
 
     def _display_welcome_message(self):
-        print("Welcome to Rock Paper Scissors!")
+        print("Welcome to Rock Paper Scissors Lizard Spock!")
 
     def _display_goodbye_message(self):
-        print("Thanks for playing Rock Paper Scissors. Goodbye!")
+        print("Thanks for playing Rock Paper Scissors Lizard Spock. Goodbye!")
 
     def _human_wins_round(self):
         human_move = self._human.move
         computer_move = self._computer.move
 
-        return ((human_move == 'rock' and computer_move == 'scissors') or
-                (human_move == 'paper' and computer_move == 'rock') or
-                (human_move == 'scissors' and computer_move == 'paper'))
+        return computer_move in self.WINNING_COMBINATIONS[human_move]
 
     def _computer_wins_round(self):
         human_move = self._human.move
         computer_move = self._computer.move
 
-        return ((computer_move == 'rock' and human_move == 'scissors') or
-                 (computer_move == 'paper' and human_move == 'rock') or
-                 (computer_move == 'scissors' and human_move == 'paper'))
+        return human_move in self.WINNING_COMBINATIONS[computer_move]
 
     def _increment_score(self):
         if self._human_wins_round():
@@ -98,6 +102,7 @@ class RPSGame:
 
         while True:
             self._reset_scores()
+
             while True:
                 self._human.choose()
                 self._computer.choose()
